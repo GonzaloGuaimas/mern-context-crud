09c4035484646cc59dc949a513924249397051a3
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-23 13:16:38 -0300

    add list items

--- client/src/context/postContext.js
+++ client/src/context/postContext.js
@@ -1,4 +1,5 @@
-import {useState, createContext, useContext} from 'react'
+import {useState, createContext, useContext, useEffect} from 'react'
+import {getPostsRequest} from '../api/posts'
 const context = createContext()
 export const usePosts = () => {
    const cntxt = useContext(context)
@@ -6,8 +7,20 @@ export const usePosts = () => {
 }
 export const PostProvider = ({children}) => {
     const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+        const res = await getPostsRequest()
+        setPosts(res.data)
+        console.log(res)
+    }
+    useEffect(() => {
+        getPosts()
+      },[])
+    
+
     return <context.Provider value={{
-        posts
+        posts,
+        getPosts
     }}>
         {children}
     </context.Provider>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {usePosts} from '../context/postContext'
+import {VscEmptyWindow} from 'react-icons/vsc'
 export const HomePage = () => {
+  const {posts} = usePosts()
+
+  if(posts.lenght ===0) return (
+    <div className='flex flex-col justify-center items-center'>
+      <VscEmptyWindow className='w-48 h-48 text-white '/>
+      <h1 className='text-white text-2xl'>No Post Yet</h1>
+    </div>
+  )
+
   return (
-    <div>
+    <div className='text-white'>
       <h2>HomePage</h2>
+      {posts.map((post) => (
+        <div key={post._id} >{post.title}</div>
+      ))}
+
     </div>
   )
 }
