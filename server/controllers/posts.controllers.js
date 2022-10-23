541b66b74273488fba1b4bf5cf2e37ffc42d0d7d
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-22 23:44:33 -0300

    add error handler

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,28 +1,52 @@
 import Post from '../models/Post.js'
 export const getPosts = async (req, res) => {
-    const posts = await Post.find()
-    return res.json(posts)
+    try{
+      const posts = await Post.find()
+      return res.json(posts)
+    }catch(error){
+      return res.status(400).json({message: error.message})
+    }
 }
  export const createPost = async (req, res) => {
-    const {title, description} = req.body
-    const newPost = new Post({title, description})
-    console.log(newPost)
-    await newPost.save()
-    return res.json(newPost)
+   try{
+      const {title, description} = req.body
+      const newPost = new Post({title, description})
+      console.log(newPost)
+      await newPost.save()
+      return res.json(newPost)
+   }catch(error){
+     return res.status(400).json({message: error.message})
+   }
+   
 }
  export const updatePost = async (req, res) => {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true} )
-    console.log(post)
-    return res.send('updated')
+   try{
+      const post = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true} )
+      console.log(post)
+      return res.json(post)
+   }catch(error){
+     return res.status(400).json({message: error.message})
+   }
+   
  }
  export const deletePost = async (req, res) =>{
-    const post = await Post.findByIdAndDelete(req.params.id)
-    if(!post) return res.status(404).send('Not Found')
-    return res.status(204).send('deleted')
+   try{
+      const post = await Post.findByIdAndDelete(req.params.id)
+      if(!post) return res.status(404).send('Not Found')
+      return res.status(204).send('deleted')
+   }catch(error){
+     return res.status(400).json({message: error.message})
+   }
+    
  }
  export const getPost = async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    if(!post) return res.status(404).send('Not Found')
-    console.log(post)
-    return res.json(post)
+   try{
+      const post = await Post.findById(req.params.id)
+      if(!post) return res.status(404).send('Not Found')
+      console.log(post)
+      return res.json(post)
+   }catch(error){
+     return res.status(400).json({message: error.message})
+   }
+    
  }
